5963bf90d1ff402405f1afb5fe57265c284fdb72
Nirbhay97 <83468600+Nirbhay97@users.noreply.github.com>
2021-11-18 18:14:18 +0530

    contact updated

--- send_mail.php
+++ send_mail.php
@@ -9,9 +9,9 @@ $webmaster_email = "ieee.ias@vit.ac.in";
 This bit sets the URLs of the supporting pages.
 If you change the names of any of the pages, you will need to change the values here.
 */
-$feedback_page = "contact.html";
-$error_page = "error_message.html";
-$thankyou_page = "thank_you.html";
+$feedback_page = "index.html";
+$error_page = "index.html";
+$thankyou_page = "index.html";
 
 /*
 This next bit loads the form field data into variables.
