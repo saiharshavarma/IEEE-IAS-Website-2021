23200165719896b199b52ab0a96bf5a0cec21f21
Hritam Dutta <hritamloyola10@gmail.com>
2021-11-28 07:07:10 +0530

    Navbar Links Made Active with Scroll, Deleted Code

--- js/navbar.js
+++ js/navbar.js
@@ -21,3 +21,23 @@ function myFunction() {
     var scrolled = (winScroll / height) * 100;
     document.getElementById("myBar").style.width = scrolled + "%";
 }
+
+// Navbar links turn active on scrolling 
+const sections = document.querySelectorAll(".scroll-section");
+const Lia = document.querySelectorAll(".navbar .topnav li a");
+window.onscroll = () => {
+    var current = "";
+    
+    sections.forEach((section) => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight= section.offsetHeight;
+        if (pageYOffset >= sectionTop - (sectionHeight / 4)) 
+            current = section.getAttribute("id"); 
+    });
+
+    Lia.forEach((a) => {
+        a.classList.remove("active");
+        if (a.classList.contains(current)) 
+            a.classList.add("active");        
+    });
+};
